bc326fd808cbbfbb329aca8b4ec709bc01c1db39
cptrobie <mike@robitaille.com>
2018-03-24 13:53:48 -0400

    Added Content, HeaderBar components, asset imagery and styling

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
+import { ContentComponent } from './content/content.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    FrameworkBodyComponent
+    FrameworkBodyComponent,
+    ContentComponent,
+    HeaderBarComponent
   ],
   exports: [
     FrameworkBodyComponent
